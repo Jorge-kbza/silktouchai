e8df35d18ee0b0d2c3d49ff132ebd7f58e417d7d
jorge <jorgecabeza184@gmail.com>
2025-08-07 10:19:54 +0200

    SilkTouchAI 1.4.1

--- main.py
+++ main.py
@@ -80,4 +80,6 @@ def devolver_archivo():
 
 if __name__ == "__main__":
     print("HOLA MUNDO SILKTOUCH AI OPERATIVO AAAAAAAAAAAAAAAa")
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
